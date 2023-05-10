4a01ee90bc3ae6012d9db7f70557ffba837c6402
Adam <phazei@gmail.com>
2023-05-10 12:21:46 -0700

    Removed uneeded code, made about look better

--- app/src/main/java/com/phazei/dynamicgptchat/settings/AppSettingsFragment.kt
+++ app/src/main/java/com/phazei/dynamicgptchat/settings/AppSettingsFragment.kt
@@ -6,15 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.phazei.dynamicgptchat.databinding.FragmentAppSettingsBinding
-import kotlinx.coroutines.Job
 
 class AppSettingsFragment : Fragment() {
 
     private var _binding: FragmentAppSettingsBinding? = null
     private val binding get() = _binding!!
-
-    private var saveApiKeyJob: Job? = null
-    private val debounceDuration = 300L
 
     override fun onCreateView(
         inflater: LayoutInflater,
