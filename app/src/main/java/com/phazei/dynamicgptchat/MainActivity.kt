97883f60ca2b3b3c7848694b3608d82fa2ee7d21
Adam <phazei@gmail.com>
2023-05-10 02:53:13 -0700

    Added about page fragment with custom menu.
    Adjusted some menu handling.

--- app/src/main/java/com/phazei/dynamicgptchat/MainActivity.kt
+++ app/src/main/java/com/phazei/dynamicgptchat/MainActivity.kt
@@ -3,18 +3,21 @@ package com.phazei.dynamicgptchat
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -26,14 +29,11 @@ import com.google.android.material.navigation.NavigationView
 import com.phazei.dynamicgptchat.chatnodes.ChatNodeViewModel
 import com.phazei.dynamicgptchat.data.datastore.Theme
 import com.phazei.dynamicgptchat.data.repo.AppSettingsRepository
-import com.phazei.dynamicgptchat.data.repo.OpenAIRepository
 import com.phazei.dynamicgptchat.databinding.ActivityMainBinding
-import com.phazei.dynamicgptchat.settings.AppSettingsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -41,15 +41,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     private val sharedViewModel: SharedViewModel by viewModels()
-    //this needs to be bound to activity so it will stay active when switching fragments
+    // this needs to be bound to activity so it will stay active when switching fragments
     private val chatNodeViewModel: ChatNodeViewModel by viewModels()
     @Inject lateinit var appSettingsRepository: AppSettingsRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //sharedViewModel must be in onCreate or it will not be created in time for the fragment
-        //leaving sharedViewModelFactory in case it's every needed in activityViewModels() in a fragment
+        // sharedViewModel must be in onCreate or it will not be created in time for the fragment
+        // leaving sharedViewModelFactory in case it's every needed in activityViewModels() in a fragment
         sharedViewModel
         chatNodeViewModel
 
@@ -68,19 +68,51 @@ class MainActivity : AppCompatActivity() {
             setOf(
                 R.id.ChatTreeListFragment,
                 R.id.prompts_graph,
-                R.id.app_settings_fragment
+                R.id.AppSettingsFragment,
+                R.id.AboutFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        binding.AboutFragment.setOnClickListener {
+            // This has the potential to really screw with the backStack
+            // It's essential that About be in the menu even though it's not visible to properly
+            //   have a topLevel menu item.
+
+            // navView.setCheckedItem(R.id.AboutFragment)
+            navView.menu.performIdentifierAction(R.id.AboutFragment, Menu.FLAG_ALWAYS_PERFORM_CLOSE)
+            navController.navigate(R.id.AboutFragment, null, NavOptions.Builder()
+                    //navigating to
+                .setEnterAnim(R.anim.spin_in_crazy)
+                .setExitAnim(R.anim.spin_out_crazy)
+                    //hitting back
+                .setPopEnterAnim(R.anim.spin_in_crazy)
+                .setPopExitAnim(R.anim.spin_out_crazy)
+                .setLaunchSingleTop(true)
+                .setRestoreState(false)
+                .setPopUpTo(R.id.AboutFragment, true)
+                .build()
+            )
+        }
+
         setupFABPageChangeFixes(navController)
 
-        // navController.addOnDestinationChangedListener { _, destination, _ ->
-        //     printNavBackStack(navController)
-        // }
-
-
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            drawerLayout.closeDrawer(GravityCompat.START)
+            //manually set active view display for "about" menu
+            val typedValue = TypedValue()
+            if (destination.id == R.id.AboutFragment) {
+                binding.root.context.theme.resolveAttribute(com.google.android.material.R.attr.colorPrimary, typedValue, true)
+                binding.AboutFragment.setTextColor(typedValue.data)
+                binding.aboutMenuBg.visibility = View.VISIBLE
+            } else {
+                binding.root.context.theme.resolveAttribute(com.google.android.material.R.attr.colorOnSurface, typedValue, true)
+                binding.AboutFragment.setTextColor(typedValue.data)
+                binding.aboutMenuBg.visibility = View.GONE
+            }
+            // printNavBackStack(navController)
+        }
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 appSettingsRepository.themeFlow.collect { theme ->
@@ -114,6 +146,7 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
+                R.id.PromptsFragment,
                 R.id.ChatTreeListFragment -> {
                     fab.show()
                     updateAppBarScrollFlags(true)
@@ -125,7 +158,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         fab.setOnClickListener {
-            //this enables the FAB to be clicked and trigger a method that can be assigned in any Fragment
+            // this enables the FAB to be clicked and trigger a method that can be assigned in any Fragment
             sharedViewModel.onFabClick.value?.invoke()
         }
     }
@@ -134,7 +167,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         if (menu is MenuBuilder) {
-            //by default it will hide any icons in the menu dropdown
+            // by default it will hide any icons in the menu dropdown
             menu.setOptionalIconsVisible(true)
         }
         return true
@@ -145,7 +178,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.app_settings_fragment -> true
+            R.id.AppSettingsFragment -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
---
+++ app/src/main/java/com/phazei/dynamicgptchat/about/AboutFragment.kt
@@ -0,0 +1,29 @@
+package com.phazei.dynamicgptchat.about
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.phazei.dynamicgptchat.R
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
