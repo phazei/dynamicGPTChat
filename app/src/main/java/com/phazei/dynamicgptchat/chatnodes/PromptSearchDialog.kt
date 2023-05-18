82babb9db32b2bb1488761f21144a1a5cefb85ad
Adam <phazei@gmail.com>
2023-05-18 14:26:09 -0700

    Added ability to chat system message to easily load prompt from prompt list

---
+++ app/src/main/java/com/phazei/dynamicgptchat/chatnodes/PromptSearchDialog.kt
@@ -0,0 +1,41 @@
+package com.phazei.dynamicgptchat.chatnodes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.phazei.dynamicgptchat.R
+import com.phazei.dynamicgptchat.databinding.DialogPromptSearchBinding
+import com.phazei.dynamicgptchat.prompts.PromptsListFragment
+
+
+class PromptSearchDialog(private val selectedListener: PromptsListFragment.OnPromptSelectedListener) : BottomSheetDialogFragment() {
+
+    private var _binding: DialogPromptSearchBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var promptsListFragment: PromptsListFragment
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogPromptSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        promptsListFragment = childFragmentManager.findFragmentById(R.id.prompts_list_fragment) as PromptsListFragment
+        promptsListFragment.setOnPromptSelectedListener(selectedListener)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
